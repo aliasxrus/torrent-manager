2e06a33256b0069d92426053e2cf7c97ced41176
colakola <ColaKola@users.noreply.github.com>
2021-05-09 01:11:10 +0300

    Update START.bat

--- START.bat
+++ START.bat
@@ -1,1 +1,25 @@
+@echo off
+
+set file1=version.dat
+set file2=package.json
+set ver=0.0.0
+
+if exist %file1% set /p ver=<%file1%
+
+set verstr=\"version\"\: \"%ver%\"
+
+findstr /ic:"%verstr%" %file2%>nul 2>&1
+if errorlevel 1 (
+ goto searchError
+) else (
+ goto searchSucces
+)
+
+:searchError
+echo Updating and reinstalling npm packages...
 npm i && node src/index.js
+goto eof
+
+:searchSucces
+echo Starting script...
+node src/index.js
