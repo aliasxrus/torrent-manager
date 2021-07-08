58ab53bd35c9a05a9b56dce8a0d5772f56997c31
Kharis.Shamsuarov <Kharis.Shamsuarov@kaspersky.com>
2021-07-08 23:28:07 +0300

    fix

--- START.sh
+++ START.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+git --version
+echo "NodeJS version:"
+node -v
 node install.js
 node src/index.js
 echo "Произошла ошибка, нажмите ввод для выхода..."
--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-npm install
-echo "Нажмите ввод для выхода..."
-read
